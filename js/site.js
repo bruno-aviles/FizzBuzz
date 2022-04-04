52bc2b082f0b143c82f588ceebb488837d0cc7de
Bruno Aviles <bruno.aviles@yahoo.com>
2022-04-04 11:51:54 -0500

    CoderFoundry solution on FizzBuzz code

--- js/site.js
+++ js/site.js
@@ -1,61 +1,85 @@
-//Display Results to user
-function displayResults(){
+// get values from the user. We need to get the fizz and the buzz value.
+function getValues(){
 
-    //get user inputs
-    let fNumber = document.getElementById("fizzNumber").value;
-    let bNumber = document.getElementById("buzzNumber").value;
-    
-    verifyNumbers(fNumber,bNumber);
+    // get the user values from the page
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+
+    // parse for numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+
+    // check that the numbers are integers
+    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        // we call fizzbuzz
+        let fBArray = fizzBuzz(fizzValue, buzzValue);
+         // call displayData and write the values to the scream
+        displayData(fBArray);
+    } else {
+        alert("You must enter an integer");
+    }
 }
 
-//Verify FIZZBUZZ
-function verifyNumbers(fizz,buzz){
+// do fizz buzz
+function fizzBuzz(fizzValue, buzzValue){
 
-    let displayNumbers = document.getElementById("results");
-    // store all <td> elements from for loop
-    let templateRows = "";
+    let returnArray = [];
 
-    // verifies to see if user inputs are between 1 to 100
-    if(fizz < 1 || fizz > 100){
-        templateRows += `<tr><td>Enter A Number Between 1 - 100</td></tr>`;
-        return displayNumbers.innerHTML = templateRows;
-    } else if(buzz < 1 || buzz > 100){
-        templateRows += `<tr><td>Enter A Number Between 1 - 100</td></tr>`;
-        return displayNumbers.innerHTML = templateRows;
+    // loop from 1 to 100
+    for (let i = 1; i <= 100; i++) {
+        // check to see if divisible by both (3 and 5)
+        // check to see if divisible by fizz value (3)
+        // check to see if divisible by buzz value (5)
+        // if none then push the number into the array
+        if(i % fizzValue == 0  && i % buzzValue){
+            returnArray.push('FizzBuzz');
+        } else if(i % 3 == 0){
+            returnArray.push('Fizz');
+        } else if(i % 5 == 0){
+            returnArray.push('Buzz');
+        } else{
+            returnArray.push(i);
+        }
+    }
+    return returnArray
+
+}
+
+// loop over the array and create a tablerow for each item.
+function displayData(fbArray){
+
+    // get the table body element from the page
+    let tableBody = document.getElementById("results");
+
+    // get the template row
+    let templateRow = document.getElementById("fbTemplate");
+
+    //clear table first
+    tableBody.innerHTML = "";
+
+    for (let index = 0; index < fbArray.length; index += 5) {
+        
+        let tableRow = document.importNode(templateRow.content, true);
+
+        // grab use the ts put into array
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].classList.add(fbArray[index])
+        rowCols[0].textContent = fbArray[index];
+
+        rowCols[1].classList.add(fbArray[index + 1])
+        rowCols[1].textContent = fbArray[index + 1];
+
+        rowCols[2].classList.add(fbArray[index + 2])  
+        rowCols[2].textContent = fbArray[index + 2];
+        
+        rowCols[3].classList.add(fbArray[index + 3])
+        rowCols[3].textContent = fbArray[index + 3];
+        
+        rowCols[4].classList.add(fbArray[index + 4])
+        rowCols[4].textContent = fbArray[index + 4];
+
+        tableBody.appendChild(tableRow);
     }
 
-    // loop through input numbers counting forward and add <td> element for each number
-    if(fizz < buzz){
-        for(number=fizz;number <= buzz;number++){
-            // replace the numbers that are a multiple of 3 and 5 with "FIZZ" and "BUZZ"
-            if(number % 3 == 0 && number % 5 == 0){
-                templateRows += `<tr><td>FIZZBUZZ</td></tr>`;
-            } else if(number % 3 == 0){
-                templateRows += `<tr><td>FIZZ</td></tr>`;
-            } else if(number % 5 == 0){
-                templateRows += `<tr><td>BUZZ</td></tr>`;
-            } else {
-                templateRows += `<tr><td>${number}</td></tr>`;
-            }
-    
-            // insert all stored values from templateRows to <tbody>
-            displayNumbers.innerHTML = templateRows;      
-        }
-    } else if(fizz > buzz){
-        for(number=fizz;number >= buzz;number--){
-            // replace the numbers that are a multiple of 3 and 5 with "FIZZ" and "BUZZ"
-            if(number % 3 == 0 && number % 5 == 0){
-                templateRows += `<tr><td>FIZZBUZZ</td></tr>`;
-            } else if(number % 3 == 0){
-                templateRows += `<tr><td>FIZZ</td></tr>`;
-            } else if(number % 5 == 0){
-                templateRows += `<tr><td>BUZZ</td></tr>`;
-            } else {
-                templateRows += `<tr><td>${number}</td></tr>`;
-            }
-    
-            // insert all stored values from templateRows to <tbody>
-            displayNumbers.innerHTML = templateRows;      
-        }
-    }    
+    // add all the rows to the table
 }
